b631f21ed71b11d1cdcc6d36420f731165475398
omirkorgen <zhumanali2021@gmail.com>
2022-08-09 18:04:43 +0600

    add homework 2 lesson

---
+++ lesson2_3_2.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import os
+import math
+
+
+try:
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+    new_window = browser.window_handles[1]
+    first_window = browser.window_handles[0]
+    browser.switch_to.window(new_window)
+    time.sleep(2)
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+
+    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    x = x_element.text
+    y = calc(x)
+    print(y)
+    answer1 = browser.find_element(By.CSS_SELECTOR, "#answer")
+    answer1.send_keys(y)
+
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+    # time.sleep(1)
+    # confirm = browser.switch_to.alert
+    # confirm.accept()
+    #
+    # def calc(x):
+    #     return str(math.log(abs(12 * math.sin(int(x)))))
+    #
+    # x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    # x = x_element.text
+    # y = calc(x)
+    # answer1 = browser.find_element(By.CSS_SELECTOR, "#answer")
+    # answer1.send_keys(y)
+    #
+    # button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    # button.click()
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
